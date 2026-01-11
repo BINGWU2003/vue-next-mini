43f478f3182b96bea0912bea70f872703984f1d1
hujiacheng <hujiacheng2003@163.com>
2026-01-11 22:45:42 +0800

    feat: 添加 ref-shallow.html 示例，展示响应式引用的使用；更新 ref.ts 和 index.ts，优化响应式处理逻辑

--- packages/vue/reactivity/src/ref.ts
+++ packages/vue/reactivity/src/ref.ts
@@ -1,6 +1,7 @@
 import { Dep, createDep } from './dep';
-import { activeEffect, trackEffects } from './effect';
+import { activeEffect, trackEffects, triggerEffects } from './effect';
 import { toReactive } from './reactive';
+import { hasChanged } from '@vue-next-mini/shared';
 export interface Ref<T = any> {
   value: T;
 }
@@ -17,12 +18,18 @@ export function createRef<T = any>(value: T, shallow: boolean = false) {
   return new RefImpl(value, shallow);
 }
 /** RefImpl类
- * value 如果是对象类型，底层调用reactive进行响应式转换，本质上是包装了一个reactive对象
  * dep 依赖集合，存储使用该ref的effect
- * 当读取value上的某个属性value.xxx  ==>  先调用trackRefValue()追踪依赖，再调用reactive的get方法追踪依赖
- * 当修改value上的某个属性value.xxx = xxx  ==>  先调用reactive的set方法触发依赖，再调用triggerEffects()触发ref的依赖
+ *
+ * value 如果是复杂数据类型，底层调用reactive进行响应式转换，本质上是包装了一个reactive对象
+ * 当读取value上的某个属性value.xxx  ==>  调用reactive的get方法追踪依赖，构成依赖关系
+ * 当修改value上的某个属性value.xxx = xxx  ==>  调用reactive的set方法触发依赖
+ *
+ * value 如果是简单数据类型，直接存储该值
+ * 当读取value  ==>  直接调用trackRefValue()追踪依赖，将依赖储存到dep中
+ * 当修改value  ==>  直接调用triggerRefValue()触发依赖
  */
 export class RefImpl<T = any> {
+  private _rawValue: T;
   private _value: T;
   // 依赖集合
   public dep?: Dep = undefined;
@@ -33,15 +40,36 @@ export class RefImpl<T = any> {
     public readonly __v_isShallow: boolean
   ) {
     this._value = __v_isShallow ? value : toReactive(value);
+    this._rawValue = value;
   }
   get value() {
+    // trackRefValue 负责“对象整体替换”的响应式追踪以及“简单数据类型”的响应式追踪
     trackRefValue(this);
     return this._value;
   }
   set value(newVal: T) {
-    console.log(newVal);
+    // 判断新值和旧值是否发生变化
+    // 简单数据类型，直接对比
+    // 引用数据类型，比较引用地址 也解决了直接赋值新对象的问题
+    // 例如：
+    // const state = ref({
+    //   name: 'John',
+    //   age: 18,
+    // });
+    //state.value = { name: 'Mary', age: 100 }; // 直接赋值新对象，引用地址发生变化，视为数据变化
 
-    // this._value = newVal;
+    // 使用_rawValue而不是_value进行比较，是因为_value是响应式对象，可能存在嵌套对象，导致比较不准确
+    if (hasChanged(newVal, this._rawValue)) {
+      this._rawValue = newVal;
+      this._value = toReactive(newVal);
+      triggerRefValue(this);
+    }
+  }
+}
+
+export function triggerRefValue(ref: RefImpl) {
+  if (ref.dep) {
+    triggerEffects(ref.dep);
   }
 }
 
--- packages/vue/shared/src/index.ts
+++ packages/vue/shared/src/index.ts
@@ -30,3 +30,14 @@ export const isArray = Array.isArray;
  * 判断是否为一个对象
  */
 export const isObject = (val: unknown) => val !== null && typeof val === 'object';
+
+/**
+ * 对比两个数据是否发生了改变
+ */
+export const hasChanged = (value: any, oldValue: any): boolean => !Object.is(value, oldValue);
+
+/**
+ * 是否为一个 function
+ */
+export const isFunction = (val: unknown): val is (...args: any[]) => any =>
+  typeof val === 'function';
